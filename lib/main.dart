44456d72a32c1b4f24a7fb2d2f4935e08378d347
gorkemoa <gorkemoa35@gmail.com>
2025-08-08 17:01:57 +0300

    Gereksiz bir import kaldırıldı ve dosya düzeni güncellendi.

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'firebase_options.dart';
 import 'core/app_theme.dart';
